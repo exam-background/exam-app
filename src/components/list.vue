e9d5f60f0d53b0a97bd8477118b9e312ebdde945
SchemingCat <x18570421676@163.com>
2020-08-25 11:54:54 +0800

    整合前端查询数据

--- src/components/list.vue
+++ src/components/list.vue
@@ -1,20 +1,35 @@
 <template>
 	<div class="content_box">
-		<!--  <h1>{{categoryID}}</h1> 这里可以看我们的传递过来的分类id -->
+		<!-- <h1>{{categoryID}}</h1> 这里可以看我们的传递过来的分类id -->
 		<van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
 			<!-- <van-cell v-for="item in list" :key="item" :title="item" /> -->
-			<div class="exhibition" v-for="(item,index) in list" :key="index" :title="item" style="border-button:1px;">
+
+			<div class="exhibition" v-for="(item,index) in list" :key="index" :title="item">
+			<!-- 	<div class="img_box">
+					<img :src="item.img" class="img" />
+				</div> -->
 				<div class="box">
 					<div class="exhibition_title">
 						<h4>{{item.title}}</h4>
 					</div>
+					<div class="exhibition_content">
+						<p class="fs_13">{{item.content}}</p>
+					</div>
+					<div class="con_tag">
+						<van-tag class="tag" type="primary">标签</van-tag>
+						<van-tag class="tag" type="primary">标签</van-tag>
+						<van-tag class="tag" type="primary">标签</van-tag>
+					</div>
+
 					<div>
-						<!-- <p class="color_red fz_15">{{item.more}}</p> -->
+						<p class="color_red fz_15">{{item.more}}</p>
 						<router-link :to="{name:'detail',path:'/detail',query:{id:1}}"  tag="p" class="color_red fz_15">Read More</router-link>
 					</div>
 				</div>
 			</div>
+
 		</van-list>
+
 	</div>
 </template>
 
@@ -23,44 +38,9 @@
 		props:['categoryID'],
 		watch: {
 			categoryID: function(val) {
-				this.categoryID = val;
-				console.log(val); // 接收父组件的值
+				this.getExercise(val)
+				//console.log(val); // 接收父组件的值
 				// 然后重新list这个数组的数据渲染数据
-				this.$axios
-					.post(this.$location.getJobDayExerciseByProfessid, this.$qs.stringify(
-					{
-						id: this.categoryID
-					}
-					))
-					.then(response => {
-						this.list = this.list.concat(response.data.data);
-						console.log("就业训练查询结果---->" + JSON.stringify(this.list));
-					})
-					.catch(function(error) {
-						// 请求失败处理
-						console.log("技术训练请求处理失败");
-						console.log(error);
-					});
-				// 根据id查询就业训练所有题目
-				this.$axios
-					.post(
-					this.$location.getTechnologyDayExerciseByProfessionalId, this.$qs.stringify(
-					{
-						id: this.categoryID
-					}
-					))
-					.then(response => {
-					this.list = this.list.concat(response.data.data);
-					console.log(
-						"就业训练查询结果---->" +
-						JSON.stringify(this.TechnologyDayExerciseList)
-					);
-					})
-					.catch(function(error) {
-					// 请求失败处理
-					console.log("就业训练请求处理失败");
-					console.log(error);
-					});
 			}
 		},
 		data() {
@@ -96,7 +76,38 @@
 					}
 				}, 1000);
 			},
-
+			getExercise(id){
+				this.$axios
+					.get(this.$location.getJobDayExerciseByProfessid, {params: {id: id}})
+					.then(response => {
+						this.list = this.list.concat(response.data.data);
+						console.log("就业训练查询结果---->" + JSON.stringify(this.list));
+					})
+					.catch(function(error) {
+						// 请求失败处理
+						console.log("技术训练请求处理失败");
+						console.log(error);
+					});
+				// 根据id查询就业训练所有题目
+				this.$axios
+					.get(
+					this.$location.getTechnologyDayExerciseByProfessionalId, {params: {id: id}})
+					.then(response => {
+					this.list = this.list.concat(response.data.data);
+					console.log(
+						"技术训练查询结果---->" +
+						JSON.stringify(this.TechnologyDayExerciseList)
+					);
+					})
+					.catch(function(error) {
+					// 请求失败处理
+					console.log("就业训练请求处理失败");
+					console.log(error);
+					});
+			}
+		},
+		mounted() {
+			this.getExercise(0);
 		}
 
 	}
